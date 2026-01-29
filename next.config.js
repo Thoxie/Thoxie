57fafc38be32d0ae95781667f314826dd8134e8e
Thoxie <Paul@innitstreetwear.com>
2026-01-29 14:47:21 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname),
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
-

661aea886954d7dcf16e96623bc52a3afeddd9d4
Thoxie <Paul@innitstreetwear.com>
2026-01-31 12:20:30 -0500

    Refine comments in postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}, // Adds vendor prefixes automatically for better browser support
-    'postcss-preset-env': {} // Allows modern CSS features
+    autoprefixer: {}, // Adds vendor prefixes automatically
+    'postcss-preset-env': {} // Enables modern CSS features as needed
   },
 };
